bcac3fe5f6d3f95b9111c8cdbf900a4a36c497f7
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-07-12 21:39:32 -0300

    adicionando projetos do curso do IFSP

---
+++ DespesasPessoais/app/src/main/java/com/example/despesaspessoais/MovimentoObj.java
@@ -0,0 +1,2 @@
+package com.example.despesaspessoais;public class MovimentoObj {
+}
---
+++ DespesasPessoais/app/src/main/java/com/example/despesaspessoais/Movimentos.java
@@ -0,0 +1,42 @@
+package com.example.despesaspessoais;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.despesaspessoais.databinding.ActivityMovimentosBinding;
+
+public class Movimentos extends AppCompatActivity {
+
+    private ActivityMovimentosBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMovimentosBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+        FloatingActionButton fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null)
+                        .setAnchorView(R.id.fab).show();
+            }
+        });
+    }
+}
